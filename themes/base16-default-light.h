0354bbd7b29a75dafe29d6dc8a46887ea3264f80
Jonathan Teran <jonathan.nerat@gmail.com>
2020-06-20 14:48:24 -0300

    initial commit

---
+++ themes/base16-default-light.h
@@ -0,0 +1,31 @@
+/*
+ * Base16: https://github.com/chriskempson/base16
+ */
+
+#define THEME_BACKGROUND "#f8f8f8"
+#define THEME_FOREGROUND "#383838"
+
+/*
+ * Colors
+ */
+#define THEME_BLACK "#f8f8f8"
+#define THEME_RED "#ab4642"
+#define THEME_GREEN "#a1b56c"
+#define THEME_YELLOW "#f7ca88"
+#define THEME_BLUE "#7cafc2"
+#define THEME_MAGENTA "#ba8baf"
+#define THEME_CYAN "#86c1b9"
+#define THEME_WHITE "#383838"
+
+/*
+ * Bright colors
+ */
+
+#define THEME_BRIGHTBLACK "#b8b8b8"
+#define THEME_BRIGHTRED "#ab4642"
+#define THEME_BRIGHTGREEN "#a1b56c"
+#define THEME_BRIGHTYELLOW "#f7ca88"
+#define THEME_BRIGHTBLUE "#7cafc2"
+#define THEME_BRIGHTMAGENTA "#ba8baf"
+#define THEME_BRIGHTCYAN "#86c1b9"
+#define THEME_BRIGHTWHITE "#181818"
